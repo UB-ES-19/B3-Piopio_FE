dfe805b9c7bba29b1a10064c81369507ea3bd968
Cristopher Q <cristopq29@gmail.com>
2019-10-01 11:57:55 +0200

    Fix Api service and Form

--- PiopioFE/src/app/app-routing.module.ts
+++ PiopioFE/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -7,7 +7,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
   { path: 'signup', component: SignUpComponent },
- 
+
 ];
 
 @NgModule({
--- PiopioFE/src/app/sign-up/sign-up.component.ts
+++ PiopioFE/src/app/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
-import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,47 +10,32 @@ import { ApiService } from '../services/api.service';
 })
 export class SignUpComponent implements OnInit {
 
-  formSignUp:FormGroup;
+  formSignUp: FormGroup;
 
-  constructor(private apiService:ApiService, private fb:FormBuilder) { }
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.createForm("","","","","","");
+    this.createForm();
   }
 
-  createForm(username,email,password,repeatedPassword,name,surname){
+  createForm() {
     this.formSignUp = this.fb.group({
-      'userName':[username,[
-        Validators.required
-      ]],
-      'email':[email,[
-        Validators.required
-      ]],
-      'password':[password,[
-        Validators.required
-      ]],
-      'rePassword':[repeatedPassword,[
-        Validators.required
-      ]],
-      'name':[name,[
-        Validators.required
-      ]],
-      'surname':[surname,[
-        Validators.required
-      ]]
-
-    })
-
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirm_password: ['', [Validators.required]],
+      profile: this.fb.group({
+        first_name: ['', [Validators.required]],
+        last_name: ['', [Validators.required]]
+      })
+    });
   }
 
-  signUp(){
-
-    this.apiService.signUp(this.formSignUp.controls['userName'].value,this.formSignUp.controls['email'].value,
-    this.formSignUp.controls['password'].value,this.formSignUp.controls['rePassword'].value,
-    this.formSignUp.controls['name'].value,this.formSignUp.controls['surname'].value).then((dato:any)=>{
-      console.log(dato);
-    })
-
+  signUp(user: FormGroup): void {
+    this.authService.signUp(user.value).subscribe(
+    response => console.log(response),
+    error => console.log(error.error) // TODO: Show errors in the form
+    );
   }
 
 }
