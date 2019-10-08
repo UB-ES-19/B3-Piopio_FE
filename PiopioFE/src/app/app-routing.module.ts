58ed25533a42fe3b9c40260cec351e2ed83e861a
Jaume Campderros <jaume.cvila@gmail.com>
2019-10-08 20:26:53 +0200

    I6 done, added two buttons for login and signin

--- PiopioFE/src/app/app-routing.module.ts
+++ PiopioFE/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  //{ path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: '', component: LandingPageComponent},
   { path: 'signup', component: SignUpComponent },
-
 ];
 
 @NgModule({
